69dbac3bd2134619bbf7a86ef0cfc068cb98b3e0
Marly-Yasuke <marly.diallo2021@gmail.com>
2022-09-16 15:28:02 +0200

    working searchbar

--- photo-app-frontend/src/components/Searchbar.jsx
+++ photo-app-frontend/src/components/Searchbar.jsx
@@ -1,27 +1,38 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import '../App.css'
+import { Link } from "react-router-dom";
 import SearchIcon from '@mui/icons-material/Search';
 import CloseIcon from '@mui/icons-material/Close';
+import axios from 'axios'
 // import BookData from '../Data.json'
 
-function Searchbar ({placeHolderSearch, data}) {
+function Searchbar ({placeHolderSearch}) {
     const [filterData, setFilteredData] = useState([]);
     const [wordEntered, setWordEntered] = useState("");
 
+useEffect(() => {
+
+  axios.get('https://lets-shoot.herokuapp.com/api/user?username=' + wordEntered)
+    .then(res => {
+      console.log(res.data)
+    })
+    .catch(e => console.log(e))
+
+},[wordEntered])
     const handleFilter = (event) =>{
       const searchWord =  event.target.value
       setWordEntered(searchWord)
     //   filtering logic of the searchbar
-    console.log(data)
-      const newFilter = data.filter((value) =>{
-        return value.title.toLowerCase.includes(searchWord.toLowerCase);
-      });
-      if (searchWord === ""){
-        setFilteredData([])
-      }else {
-        setFilteredData(newFilter);
+    
+      // const newFilter = data.filter((value) =>{
+      //   return value.title.toLowerCase.includes(searchWord.toLowerCase);
+      // });
+    //   if (searchWord === ""){
+    //     setFilteredData([])
+    //   }else {
+    //     setFilteredData(newFilter);
   
-    }
+    // }
     }
     const clearInput = ()=>{
         setFilteredData([]);
@@ -42,9 +53,9 @@ return (
             return (
                 //                {/* value.title = the value parameter and the value wanted */}
 
-                <a className="dataItem" href={"value.link"}>
+                <Link className="dataItem" to={value.link}>
             <p>{value.title}</p>
-            </a>
+            </Link>
         )})}
     </div>
     </div>
