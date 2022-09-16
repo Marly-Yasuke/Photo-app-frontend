0c32095be8fd0f6f069fef08bc2e9d93d414e2b8
Marly-Yasuke <marly.diallo2021@gmail.com>
2022-09-16 15:46:09 +0200

    last searchbar

--- photo-app-frontend/src/App.js
+++ photo-app-frontend/src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
      <header>
-      <Searchbar placeHolderSearch={"Search"} data={""}/>
+      <Searchbar placeHolderSearch={"Search"}/>
       <Navbar/>
      </header>
      <Routes>
