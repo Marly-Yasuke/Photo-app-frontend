dd28ae3c4b68aca9a14016f2e66bf87a8c26bd98
Marko Ivezic <markoivezic89@gmail.com>
2022-09-20 09:18:30 +0200

    Login logic

--- photo-app-frontend/src/context/auth.context.jsx
+++ photo-app-frontend/src/context/auth.context.jsx
@@ -13,27 +13,24 @@ function AuthProviderWrapper(props) {
       localStorage.setItem('authToken', token);
     }
 
-    const authenticateUser = () => {           //  <==  ADD  
+    const authenticateUser = () => { 
         // Get the stored token from the localStorage
         const storedToken = localStorage.getItem('authToken');
         // If the token exists in the localStorage
         if (storedToken) {
-          // We must send the JWT token in the request's "Authorization" Headers
           axios.get(
             `${API_URL}/auth/verify`, 
             { headers: { Authorization: `Bearer ${storedToken}`} }
           )
           .then((response) => {
             // If the server verifies that JWT token is valid  
-            const user = response.data;
-           // Update state variables        
+            const user = response.data;        
             setIsLoggedIn(true);
             setIsLoading(false);
             setUser(user);        
           })
           .catch((error) => {
-            // If the server sends an error response (invalid token) 
-            // Update state variables         
+            // If the server sends an error response (invalid token)         
             setIsLoggedIn(false);
             setIsLoading(false);
             setUser(null);        
