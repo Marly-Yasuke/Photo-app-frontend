5c1e043899f6fce06be8b57664807752d135d922
Marko Ivezic <markoivezic89@gmail.com>
2022-09-20 12:32:52 +0200

    users profile update

---
+++ photo-app-frontend/src/components/UserPictures.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UserPictures = ({id}) => {
+  return (
+    <section>
+      <h2>My Pictures</h2>
+    </section>
+  );
+};
+
+export default UserPictures;
--- photo-app-frontend/src/context/auth.context.jsx
+++ photo-app-frontend/src/context/auth.context.jsx
@@ -5,59 +5,60 @@ const API_URL = "https://lets-shoot.herokuapp.com";
 const AuthContext = React.createContext();
 
 function AuthProviderWrapper(props) {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [isLoading, setIsLoading] = useState(true);
-    const [user, setUser] = useState(null);
-  
-    const storeToken = (token) => { 
-      localStorage.setItem('authToken', token);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [user, setUser] = useState(null);
+
+  const storeToken = (token) => {
+    localStorage.setItem("authToken", token);
+  };
+
+  const authenticateUser = () => {
+    // Get the stored token from the localStorage
+    const storedToken = localStorage.getItem("authToken");
+    // If the token exists in the localStorage
+    if (storedToken) {
+      axios
+        .get(`${API_URL}/api/auth/verify`, {
+          headers: { Authorization: `Bearer ${storedToken}` },
+        })
+        .then((response) => {
+          // If the server verifies that JWT token is valid
+          const user = response.data;
+          setIsLoggedIn(true);
+          setIsLoading(false);
+          setUser(user);
+        })
+        .catch((error) => {
+          // If the server sends an error response (invalid token)
+          setIsLoggedIn(false);
+          setIsLoading(false);
+          setUser(null);
+        });
+    } else {
+      // If the token is not available (or is removed)
+      setIsLoggedIn(false);
+      setIsLoading(false);
+      setUser(null);
     }
+  };
+  useEffect(() => {
+    authenticateUser();
+  }, []);
 
-    const authenticateUser = () => { 
-        // Get the stored token from the localStorage
-        const storedToken = localStorage.getItem('authToken');
-        // If the token exists in the localStorage
-        if (storedToken) {
-          axios.get(
-            `${API_URL}/auth/verify`, 
-            { headers: { Authorization: `Bearer ${storedToken}`} }
-          )
-          .then((response) => {
-            // If the server verifies that JWT token is valid  
-            const user = response.data;        
-            setIsLoggedIn(true);
-            setIsLoading(false);
-            setUser(user);        
-          })
-          .catch((error) => {
-            // If the server sends an error response (invalid token)         
-            setIsLoggedIn(false);
-            setIsLoading(false);
-            setUser(null);        
-          });      
-        } else {
-          // If the token is not available (or is removed)
-            setIsLoggedIn(false);
-            setIsLoading(false);
-            setUser(null);      
-        }   
-      }
-      useEffect(() => {                              
-      }, []);
-
-    return (
-        <AuthContext.Provider 
-            value={{ 
-                isLoggedIn,
-                isLoading,
-                user,
-                storeToken,
-                authenticateUser
-            }}
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn,
+        isLoading,
+        user,
+        storeToken,
+        authenticateUser,
+      }}
     >
       {props.children}
     </AuthContext.Provider>
-    )
+  );
 }
 
 export { AuthProviderWrapper, AuthContext };
